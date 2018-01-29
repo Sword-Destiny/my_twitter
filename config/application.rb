69191b38f50cf9564cf5f039e7bca1a9e4392bb3
天命剑主-袁浩 <yuanhonglong@outlook.com>
2018-01-29 17:24:29 +0800

    bug fix : id重复

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,9 @@ module MyTwitter
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 
 
