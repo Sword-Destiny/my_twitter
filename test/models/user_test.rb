b804754c46ab25e73f0fc14196121c256e17a99d
Bai Jiameng <baijiameng@iie.ac.cn>
2018-01-30 15:40:30 +0000

    bjm

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,4 +16,31 @@ class TestUser < ActiveSupport::TestCase
     @user.name='    '
     assert_not @user.valid?
   end
+  
+  test "name should not be too long" do
+    @user.name = aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    assert_not @user.valid?
+  end
+  
+  test "user should have picture" do
+    @user = User.register('bai',123456)
+    assert_equal '/heads/default.png',  @user[:head_picture]
+  end
+  
+  test "user should have the default message from " do
+    assert_equal 'Wellcome to our website!', @user.im_info[1]
+  end
+  
+  test "name should be update if change the name" do
+    @user.update_name(11,'bai1')
+    assert_equal 'bai1', @user.name
+    
+  end
+  
+  test "password should be update if change the password" do
+    @user.update_password(self,'1234567')
+    assert_equal '1234567',@user.password
+  end
+  
+
 end
